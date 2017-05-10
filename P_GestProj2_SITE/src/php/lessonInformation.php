9991f6d2b575a3b4f158f27f272617044453a53f
colombofa <colombofa@etml.educanet2.ch>
2017-05-10 14:39:48 +0200

    Information cours 1
    Création de la page + affichage de base

---
+++ P_GestProj2_SITE/src/php/lessonInformation.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: colombofa
+ * Date: 10.05.2017
+ * Time: 14:32
+ */
+
+?>
+<!--PDO-->
+<?php
+include ("PDOLink.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include("include/head.php");# Inclu le fichier head.php?>
+</head>
+
+<body>
+
+<div id="wrapper">
+    <!-- Navigation -->
+    <nav>
+        <?php include("include/nav.php");# Inclu le fichier nav.php?>
+    </nav>
+    <div id="page-wrapper">
+        <div class="container-fluid">
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Cours Proposés
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="src/js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="src/js/bootstrap.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="src/js/plugins/morris/raphael.min.js"></script>
+    <script src="src/js/plugins/morris/morris.min.js"></script>
+    <script src="src/js/plugins/morris/morris-data.js"></script>
+
+</body>
+
+</html>
