eb9faf338d04c7a2cb433b38c8e9e2992e3d4b9c
André Gomes <gomesan@etml.educanet2.ch>
2017-05-10 14:38:38 +0200

    Commit 1 AGS
    Ajout des informations en plus nécessaire pour la base de donnée

--- P_GestProj2_SITE/src/php/creationCompte.php
+++ P_GestProj2_SITE/src/php/creationCompte.php
@@ -37,6 +37,39 @@ include ("PDOLink.php");
                     <!-- Formulaire pour la création de compte -->
                    <form name="creationCompte" action="#" method="post">
                        <p>
+                           Nom: </br>
+                           <input type="text" name="lastname" >
+                       </p>
+                       <p>
+                           Prénom: </br>
+                           <input type="text" name="firstname" >
+                       </p>
+                       <p>
+                           Adresse: </br>
+                           <input type="text" name="adress" >
+                       </p>
+                       <p>Email: </br>
+                           <input type="text" name="mail" >
+                       </p>
+                       <p>
+                           Téléphone: </br>
+                           <input type="text" name="phoneNumber" >
+                       </p>
+                       <p>
+                           Vous êtes: </br>
+                           <input type="radio" name="type" value="1"> Enseignant
+                           <br>
+                           <input type="radio" name="type" value="2"> Etudiant
+                       </p>
+                       <p>
+                           Profession: </br>
+                           <input type="text" name="prof" >
+                       </p>
+                       <p>
+                           AVS: </br>
+                           <input type="text" name="avsCode" >
+                       </p>
+                       <p>
                            Login: </br>
                            <input type="text" name="loginName" >
                        </p>
@@ -51,6 +84,26 @@ include ("PDOLink.php");
                        <?php
                         if(isset($_POST["btnSend"]))
                         {
+                            //Partie de la requête des utilisateurs
+                            //Création des variables
+                            $connector = new PDOLink();
+
+                            //Regarde qu'elle est le type de l'enregistrement pour définir ou va la requête sql
+                            if($_POST['type'] == 1) {
+                                $query = 'INSERT INTO `t_former`(`forLastname`, `forFirstname` , `forLocation`, `forEmail` , `forPhoneNumber` , `forProfession` , `forAVSNumber) VALUES ("' . $_POST['lastname'] . '","' . $_POST['firstname'] . '",
+                                "' . $_POST['adress'] . '","' . $_POST['mail'] .'","'. $_POST['phoneNumber'].'","'. $_POST['prof'] . '","' . $_POST['avsCode'] .'")';
+                            }else{
+                                $query = 'INSERT INTO `t_student`(`stuLastname`, `stuFirstname` , `stuLocation`, `stuEmail` , `stuPhoneNumber` , `stuProfession` , `stuAVSNumber) VALUES ("' . $_POST['lastname'] . '","' . $_POST['firstname'] . '",
+                                "' . $_POST['adress'] . '","' . $_POST['mail'] .'","'. $_POST['phoneNumber'].'","'. $_POST['prof'] . '","' . $_POST['avsCode'] .'")';
+                            }
+                            //Execute la requête pour l'ajout des informations dans la table respective
+                            $req = $connector->executeQuery($query);
+                            //Ferme et détruis la requête en cours pour liberer l'espace de stockage
+                            $connector->closeCursor($req);
+                            $connector->destructObject();
+
+
+                            //Partie de la requête du compte
                             //Création des variables
                             $connector = new PDOLink();
                             $login = $_POST["loginName"];
